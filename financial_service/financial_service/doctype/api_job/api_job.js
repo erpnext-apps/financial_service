6027304f7b64a4fc5ad72755a85bc07c94766302
Deepesh Garg <deepeshgarg6@gmail.com>
2021-11-10 11:32:19 +0530

    feat: Add API JOB doctype for queuing API requests

---
+++ financial_service/financial_service/doctype/api_job/api_job.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, Frappe Technologies Pvt. Ltd. and Contributors and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('API Job', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ financial_service/financial_service/doctype/api_job/test_api_job.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2021, Frappe Technologies Pvt. Ltd. and Contributors and Contributors
+# See license.txt
+
+# import frappe
+import unittest
+
+class TestAPIJob(unittest.TestCase):
+	pass
